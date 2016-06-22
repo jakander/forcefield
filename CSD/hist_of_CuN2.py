485c2d9a89f4523277ed68011a3849780965942b
vwjake70 <jakander@colostate.edu>
2016-06-22 17:21:51 -0600

    Script that plots the data points into a histogram to get a distribution of Cu-(N)2 coord. distances

--- CSD/hist_of_CuN2.py
+++ CSD/hist_of_CuN2.py
@@ -1,4 +1,8 @@
+
+#PREAMBLE:
+
 import numpy as np
+from scipy import stats
 import sys
 import os
 import matplotlib.pyplot as plt
@@ -11,9 +15,18 @@ nullfmt = NullFormatter()
 
 dat1 = sys.argv[1]
 system = sys.argv[2]
+sel = []
+sel.append(['bond dist','firebrick'])
+
+t = "Mean= 2.044"\
+	" Mode= 2.016"
 
 
-def hist1d(data, x_axis, num_b, system, analysis, norm):
+#-----------------------------------------------------------------------
+#Subroutine:
+
+
+def hist1d(data, x_axis, num_b, system, norm):
 	""" Creates a 1D histogram:
 	Usage: hist1d(data, x_axis, num_b, system, analysis, norm)
 	
@@ -25,29 +38,52 @@ def hist1d(data, x_axis, num_b, system, analysis, norm):
 	analysis: descriptor for the analysis performed and plotted
 	norm = [False][True]; if False, plotting a frequency of data; if True, plotting a probability density
 	"""
+	mode = stats.mode(data)
+	mean = np.mean(data)
+	stdev = np.std(data)
+	print 'stdev=', stdev
 	
-	events, edges, patches = plt.hist(data, bins=num_b, histtype = 'bar', normed=norm)
+	plt.hist(data, bins=num_b, histtype = 'bar', normed=norm)
 	plt.grid(b=True, which='major', axis='both', color='#808080', linestyle='--')
 	plt.xlabel(r'%s' %(x_axis), size=12)
-#	plt.title(r'something here...' %(system), size='14')
+	plt.title('From CSD for All Structures with Cu Coordinated to Two N', size='14')
+	plt.axvline(x = mean, color = "r")
+	plt.xticks(np.arange(1.3, 3.5, 0.2))
+	plt.xlim((1.7,2.7))
+	#plt.text(2.8, 8000, t, ha='left', va='top', wrap=True, fontsize=12, color='k', bbox=dict(facecolor='#D3D3D3', ec='none'))
+	#plt.text(3.0,7000, "Mode=2.016", va='top', ha='right', fontsize=12, color='k', bbox=dict(facecolor='#D3D3D3',ec='none'))
+	plt.text(2.5,8500, "Mean= 2.044" , va='top', ha='left', fontsize=12, color='k', backgroundcolor='#D3D3D3')
+	plt.text(2.5,8000, "Mode= 2.016", va='top', ha='left', fontsize=12, color='k', backgroundcolor='#D3D3D3')
 
 	if norm == True:
 		plt.ylabel('Probability Density')
-		plt.savefig('%s.%s.prob1d.png' %(system,analysis))
-		nf = open('%s.%s.prob1d.dat' %(system,analysis),'w')
+		#plt.savefig('%s.%s.prob1d.png' %(system))
+		#nf = open('%s.%s.prob1d.dat' %(system),'w')
 	else:
 		plt.ylabel('Frequency', size=12)
-		plt.savefig('%s.%s.hist1d.png' %(system,analysis))
-		nf = open('%s.%s.hist1d.dat' %(system,analysis), 'w')
-
-	for i in range(len(events)):
-		nf.write('%10.1f      %10.4f\n' %(events[i], edges[i]))
+		#plt.savefig('%s.%s.hist1d.png' %(system))
+		#nf = open('%s.%s.hist1d.dat' %(system), 'w')
 	
+	
+	plt.show()
 	plt.close()
-	nf.close()
-	events = []
-	edges = []
-	patches = []
+
+
+
+
+#-------------------------------------------------------------------------
+#MAINPROGRAM:
+
+
+datalist1 = np.loadtxt(dat1)
+data_range = np.percentile(datalist1[:,:], [0, 100])
+print data_range
+
+
+for i in range(len(sel)):
+	selection = sel[i][0]
+
+	hist1d(datalist1[:,i], 'Coordination Distance ($\AA$)', 250, '%02d.%s.%s' %(i, system, selection), False)
 
 
 
